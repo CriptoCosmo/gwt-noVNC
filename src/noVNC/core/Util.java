badbf2de97dd5d9bf4bae3d3fd47f4b5d1325e9b
vineet-sinha <vineet@architexa.com>
2011-11-22 21:28:11 -0500

    Detailed logging while we get this to work...

--- src/noVNC/core/Util.java
+++ src/noVNC/core/Util.java
@@ -72,10 +72,18 @@ public class Util {
 //	            throw("invalid logging type '" + level + "'");
 //	    }
 //	};
-	public static void Info(String msg) {}
-	public static void Debug(String msg) {}
-	public static void Warn(String msg) {}
-	public static void Error(String msg) {}
+	public static void Info(String msg) {
+		System.out.println("NFO: " + msg);
+	}
+	public static void Debug(String msg) {
+		System.err.println("DBG: " + msg);
+	}
+	public static void Warn(String msg) {
+		System.err.println("WRN: " + msg);
+	}
+	public static void Error(String msg) {
+		System.err.println("ERR: " + msg);
+	}
 //	Util.get_logging = function () {
 //	    return Util._log_level;
 //	};
