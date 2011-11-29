f55d0a659049b887b34c9c7e7007198ff64c5826
vineet-sinha <vineet@architexa.com>
2011-11-29 18:43:06 -0500

    Does not print all of long messages

--- src/noVNC/utils/DataUtils.java
+++ src/noVNC/utils/DataUtils.java
@@ -6,19 +6,25 @@ public class DataUtils {
 	public static final boolean sending = false;
 
 	public static void printData(boolean rcv, String decoded, String raw) { 
+		if (decoded.length() == 0) return;
 		if (rcv) {
 			System.err.print("Receiving: ");
 		} else {
 			System.err.print("Sending: ");
 		}
 		byte[] decodedBytes = decoded.getBytes();
-		boolean first = true;
-		for (byte b : decodedBytes) {
-			if(!first)
-				System.err.print(",");
+		
+		for (int i = 0; i<decodedBytes.length; i++) {
+			byte b = decodedBytes[i];
+			if(i != 0) System.err.print(",");
 			System.err.print(b);
-			first = false;
+			if (i>=80) {
+				System.err.print(", " + decodedBytes.length + " bytes total");
+				raw = "Large Str - len: " + raw.length();
+				break;
+			}
 		}
+
 		if (rcv) {
 			System.err.print(" <-- ");
 		} else {
@@ -29,7 +35,7 @@ public class DataUtils {
 
 	// for debugging data going from the server to the client
 	public static void printSCData(boolean rcv, String decoded, String raw) {
-		//printData(rcv, decoded, raw);
+		printData(rcv, decoded, raw);
 	}
 
 	// for debugging data going from the client to the server
